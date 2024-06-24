b6a667bd2daeb9e2d4c0c7b26cb1750bf942f584
Ebenezer Tseh <ebenezertseh2002@gmail.com>
2024-06-23 22:23:29 -0400

    completed react task tracker

---
+++ src/components/header.js
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+const Header = ({title, onAdd, showAdd}) => {
+   
+    return (
+      
+      <header className = "header">
+          <h1>{title}</h1>
+         <Button onClick = {onAdd} color = {showAdd? 'green' : 'red'} text = {showAdd? "Add" : "Close"}/>
+      
+        
+      </header>
+    )
+  }
+
+  Header.defaultProps = {
+  title : "Task Tracker"
+
+
+  }
+
+Header.propTypes = {
+
+    title : PropTypes.string
+}
+  export default Header
+
